b01a788d5e6ae81b9c01211883b9c78c8806994c
Piyush Ashish <155777627+Piyu7371@users.noreply.github.com>
2026-08-08 00:13:41 +0530

    Update JwtFilter.java

--- src/main/java/com/womensafety/women_safety_backend/security/JwtFilter.java
+++ src/main/java/com/womensafety/women_safety_backend/security/JwtFilter.java
@@ -1,17 +1,11 @@
 package com.womensafety.women_safety_backend.security;
 
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collections;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -39,7 +33,6 @@ public class JwtFilter extends OncePerRequestFilter {
 
         String authHeader = request.getHeader("Authorization");
 
-        System.out.println("JWT FILTER HIT");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             try {
@@ -57,7 +50,6 @@ public class JwtFilter extends OncePerRequestFilter {
                             );
 
                     SecurityContextHolder.getContext()
-                            .setAuthentication(authentication);
                 }
 
             } catch (Exception e) {
